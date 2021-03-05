05f66890c49204e482c475cbd3a1b8d521be5d93
yuqing521 <1581446178@qq.com>
2021-03-05 09:13:04 +0800

    update 1.3.0

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const Koa = require('koa');
 const app = new Koa();
+const path = require('path')
 const router = require('koa-router')();
 const index = require('./index')
+const views = require('koa-views')
+
+
+
+app.use(views(path.join(__dirname, './'), {
+    extension: 'ejs'
+}))
 app.use(router.routes());
+
+
 router.get('/api/captin', async (ctx, next) => {
-    const UID = ctx.request.querystring
+    const UID = ctx.request.query.uid
     console.log(UID)
-    ctx.body = await index.main(parseInt(UID))
+    const Info = await index.main(parseInt(UID))
+    await ctx.render('index', {
+        Info,
+    })
 });
 
 app.listen(3000);
